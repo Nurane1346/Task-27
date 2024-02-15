47e44eeed8d0f1c86dde02fc29443bd0c51afabe
Nurane1346 <nuraneh01@gmail.com>
2024-02-15 23:53:37 +0400

    created file

---
+++ main.js
@@ -0,0 +1,32 @@
+
+// Task-1
+// let friends=["Lale","Hecer","Aynur",]
+// friends.push("Ayten")
+// friends.shift("lale")
+// console.log(friends)
+
+
+// Task-4
+
+// let number=[2,3,4,7,12]
+// let result=number.map(item=>{
+//     return item*item
+// })
+// console.log(result)
+// console.log(number)
+
+// Task-8
+
+// let numbers=[7,-1,2,-8,5,-3,10,-6,45]
+// let result=numbers.filter(item=>{
+//     return item<0
+// })
+// console.log(result)
+
+// Task-9
+
+// let numbers=[2,-5,15,-36,85,7,-6,4,-21]
+// let result=numbers.filter(item=>item>0)
+// console.log(result)
+
+
